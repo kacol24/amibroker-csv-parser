4654a3bc4783646e969f607ac7182be65ad669a8
Kevin Chandra <kacol.bot@gmail.com>
2020-12-01 19:13:36 +0700

    filename without extension

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -5,13 +5,18 @@ namespace App\Http\Controllers;
 use App\Exports\AmibrokerExport;
 use App\Imports\AmibrokerImport;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Excel;
 
 class ImportController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $collection = (new AmibrokerImport)->toCollection($request->file('excel'), null, \Maatwebsite\Excel\Excel::CSV);
+        $filename = $request->file('excel')->getClientOriginalName();
+        $filename = pathinfo($filename, PATHINFO_FILENAME);
 
-        return (new AmibrokerExport($collection->first()))->download($request->file('excel')->getClientOriginalName() . '.csv');
+        $collection = (new AmibrokerImport)->toCollection($request->file('excel'), null, Excel::CSV);
+
+        return (new AmibrokerExport($collection->first()))
+            ->download($filename . '.csv');
     }
 }
